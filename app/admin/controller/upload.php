b34b1255563762b09fd977252b489ef03ea95b2e
cpz <214564605@qq.com>
2022-09-06 17:54:34 +0800

    上传文件 优化

--- app/admin/controller/upload.php
+++ app/admin/controller/upload.php
@@ -10,23 +10,16 @@ use support\Request;
 class upload
 {
     //主要处理上传图片
-    public function image(Request $request){
-        $data_array=array();
+    public function editor_image(Request $request){
+        $session = $request->session();
+        $admin_id=$session->get('admin_id',0);
+
         $file_path=upload_path();
-        $temp_error_array=array();//记录错误信息
-        $web_path=str_replace(public_path(),'',$file_path);
-        $list=array();
+        $image = new \Image();
+        $data=array();
         foreach ($request->file() as $key => $spl_file) {
-            /*
-            var_export($spl_file->isValid()); // 文件是否有效，例如ture|false
-            var_export($spl_file->getUploadExtension()); // 上传文件后缀名，例如'jpg'
-            var_export($spl_file->getUploadMineType()); // 上传文件mine类型，例如 'image/jpeg'
-            var_export($spl_file->getUploadErrorCode()); // 获取上传错误码，例如 UPLOAD_ERR_NO_TMP_DIR UPLOAD_ERR_NO_FILE UPLOAD_ERR_CANT_WRITE
-            var_export($spl_file->getUploadName()); // 上传文件名，例如 'my-test.jpg'
-            var_export($spl_file->getSize()); // 获得文件大小，例如 13364，单位字节
-            var_export($spl_file->getPath()); // 获得上传的目录，例如 '/tmp'
-            var_export($spl_file->getRealPath()); // 获得临时文件路径，例如 `/tmp/workerman.upload.SRliMu`
-            */
+            echo 'a2--'.PHP_EOL;
+            //if($key!='attribute_image')continue;
             //20MB的文件 拒绝上传
             $max_size=20*1024*1024;
             if($spl_file->getSize()>=$max_size){
@@ -35,19 +28,89 @@ class upload
                 continue;
             }
             $file_content=file_get_contents($spl_file->getRealPath());
-            $file_name=$spl_file->getUploadName().'_'.md5($file_content).'.'.$spl_file->getUploadExtension();
+            $file_name= uniqid().'_'.$spl_file->getUploadName();
 
-            $list[]=array(
-                'web_path'=>$web_path.'\\'.$file_name,
-                'getUploadMineType'=>$spl_file->getUploadMineType(),
-                'getSize'=>$spl_file->getSize(),
-                'getUploadExtension'=>$spl_file->getUploadExtension(),
+            $original_file_path=$file_path."\\".$file_name;
+            file_put_contents($original_file_path,$file_content);//保存原始图片
+
+            $data=array(
+                'user_id'=>$admin_id,//上传图片的用户
+                'file_path'=>$original_file_path,
+                'file_name'=>$spl_file->getUploadName(),
+                'web_path'=>web_path($original_file_path),
+                'file_size'=>$spl_file->getSize(),
+                'file_type'=>$spl_file->getUploadMineType(),
+                'file_extension'=>$spl_file->getUploadExtension(),
             );
-            file_put_contents($file_path."\\".$file_name,$file_content);
+
+            $where=array();
+            $where[]=['file_path','=',$file_path];
+            $product_image_rows=Db::table('product_image')->where($where)->find();
+            if(empty($product_image_rows)){
+                $file_id=Db::table('product_image')->insert($data,true);
+                $data['file_id']=$file_id;
+            }
+            //$file_list[]=$data;
         }
-        return json(['code' => 200, 'msg' => 'ok','data'=>$list]);
+        $return_data=[];
+        $return_data['url']=$data['web_path'];
+        $return_data['alt']=$data['file_name'];
+        $return_data['href']=$data['web_path'];
+        return json(['errno' => 0, 'msg' => 'ok','data'=>$return_data]);
     }
-    //主要处理上传图片
+
+    //编辑器上传视频
+    public function editor_video(Request $request){
+        $session = $request->session();
+        $admin_id=$session->get('admin_id',0);
+
+        $file_path=upload_path();
+        $data=array();
+        foreach ($request->file() as $key => $spl_file) {
+            echo 'a2--'.PHP_EOL;
+            //if($key!='attribute_image')continue;
+            //20MB的文件 拒绝上传
+            $max_size=20*1024*1024;
+            if($spl_file->getSize()>=$max_size){
+                $size_mb=$spl_file->getSize()/1024/1024;
+                $temp_error_array[]=$spl_file->getUploadName().'文件'.$size_mb."mb 过大!";
+                continue;
+            }
+            $file_content=file_get_contents($spl_file->getRealPath());
+            $file_name= uniqid().'_'.$spl_file->getUploadName();
+
+            $original_file_path=$file_path."\\".$file_name;
+            file_put_contents($original_file_path,$file_content);//保存原始图片
+
+            $data=array(
+                'user_id'=>$admin_id,//上传图片的用户
+                'file_path'=>$original_file_path,
+                'file_name'=>$spl_file->getUploadName(),
+                'web_path'=>web_path($original_file_path),
+                'file_size'=>$spl_file->getSize(),
+                'file_type'=>$spl_file->getUploadMineType(),
+                'file_extension'=>$spl_file->getUploadExtension(),
+            );
+
+            $where=array();
+            $where[]=['file_path','=',$file_path];
+            $product_image_rows=Db::table('product_image')->where($where)->find();
+            if(empty($product_image_rows)){
+                $file_id=Db::table('product_image')->insert($data,true);
+                $data['file_id']=$file_id;
+            }
+            //$file_list[]=$data;
+        }
+        $return_data=[];
+        $return_data['url']=$data['web_path'];
+        //$return_data['alt']=$data['file_name'];
+        //$return_data['href']=$data['web_path'];
+        //$return_data['poster']=$data['poster'];
+        return json(['errno' => 0, 'msg' => 'ok','data'=>$return_data]);
+    }
+
+
+    //上传属性图
     public function attribute_image(Request $request){
         $session = $request->session();
         $admin_id=$session->get('admin_id',0);
@@ -112,7 +175,7 @@ class upload
             }
             $file_list[]=$data;
         }
-        return json(['code' => 200, 'msg' => 'ok','data'=>$data]);
+        return json(['code' => 200, 'msg' => 'ok','data'=>$file_list]);
     }
 
 
